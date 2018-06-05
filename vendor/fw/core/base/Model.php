09df98f55565849b6d4e21e67648d87c818913c8
Andrey Margashov <Andrey.Margashov@yandex.ru>
2018-06-05 09:52:07 +0300

    Проставил комментарии в классе Model

--- vendor/fw/core/base/Model.php
+++ vendor/fw/core/base/Model.php
@@ -6,20 +6,28 @@ namespace fw\core\base;
 use fw\core\Db;
 use Valitron\Validator;
 
+/**
+ * Class Model Базовый класс модели
+ * @package fw\core\base
+ */
 abstract class Model
 {
-    private $db;
-    private $table;
-    private $pk = 'id';
+    private $db;            // менеждер БД
+    private $table;         // таблица, связанная с моделью
+    private $pk = 'id';     // поле первичного ключа (уникальный ИД объекта модели)
 
-    private $attributes = [];
-    private $errors = [];
-    private $rules = [];
+    private $attributes = [];   // аттрибуты модели, которые будут сохраняться в БД
+    private $errors = [];       // ошибки (не пусто, если что-то пошло не так)
+    private $rules = [];        // равила валидации (в формате библиотеки Valitron\Validator)
 
     public function __construct()
     {
         $this->db = Db::instance();
     }
+
+    /**************************************************************************
+     *  Методы доступа
+     **************************************************************************/
 
     public function getTable()
     {
@@ -80,11 +88,25 @@ abstract class Model
         return $this->errors;
     }
 
+
+    /**
+     * Выполнение произвольного запроса к БД (без возврата результирующего массива)
+     *
+     * @param $sql  Собственно запрос
+     * @param array $params Необязятельный массив параметров
+     * @return mixed true? если успешно, иначе - false
+     */
     public function query($sql, $params = [])
     {
         return $this->db->execute($sql, $params);
     }
 
+    /**
+     * Загрузка атрибутов модель
+     * Устанавливаются только те аттрибуты, которые "прописаны" в свойстве attributes
+     *
+     * @param array $data Массив аттрибутов для загрузки.
+     */
     public function load(array $data)
     {
         foreach ($this->attributes as $key => $value) {
@@ -94,6 +116,11 @@ abstract class Model
         }
     }
 
+    /**
+     * Сохранение аттрибутов в бд
+     *
+     * @return mixed true, если успешно
+     */
     public function save()
     {
         $sql = 'insert into ' . $this->table .  '('. implode(',',  array_keys($this->attributes)) . ')' .
@@ -101,6 +128,28 @@ abstract class Model
         return $this->db->execute($sql, array_values($this->attributes));
     }
 
+    /**
+     * Сформировать HTML код для вывода ошибок
+     *
+     * @return string HTML код
+     */
+    private function getHtmlCodeForErrors()
+    {
+        $messages = '<ul>';
+        foreach ($this->errors as $error) {
+            foreach ($error as $item) {
+                $messages .= "<li>$item</li>";
+            }
+        }
+        $messages  .= '</ul>';
+        return @$messages;
+    }
+
+    /**
+     * Валидация модели
+     *
+     * @return bool true, если успешно
+     */
     public function validate()
     {
         $validator = new Validator($this->attributes);
@@ -110,30 +159,41 @@ abstract class Model
         } else {
             $this->errors = $validator->errors();
             // сохраняем HTML код ошибок в сессии
-            $messages = '<ul>';
-            foreach ($this->errors as $error) {
-                foreach ($error as $item) {
-                    $messages .= "<li>$item</li>";
-                }
-            }
-            $messages  .= '</ul>';
-            $_SESSION['errors'] = $messages;
+            $_SESSION['errors'] = $this->getHtmlCodeForErrors();
             return false;
         }
     }
 
+    /**
+     * Получить все данные из БД
+     *
+     * @return mixed Результирующий массив данных из БД
+     */
     public function findAll()
     {
         $sql = "select * from {$this->table}";
         return $this->db->query($sql);
     }
 
+    /**
+     * Вернуть данные для объекта по ИД
+     *
+     * @param $id ИД
+     * @return mixed Массив данных из БД
+     */
     public function findById($id)
     {
         $sql = "select * from {$this->table} where {$this->pk} = ?";
         return $this->db->query($sql, [$id]);
     }
 
+    /**
+     * Вернуть массив данных из БД по значению поля
+     *
+     * @param $field Имя поля
+     * @param $value Значение пол Количество возвращаемых строк (если -1 - возвращаем все строки)
+     * @return mixed Массив возвращаемых данных
+     */
     public function findByName($field, $value, $numRows = -1)
     {
         $limit = (-1 === $numRows) ? "" : " limit $numRows";
@@ -141,11 +201,25 @@ abstract class Model
         return $this->db->query($sql, [$value, $limit]);
     }
 
+    /**
+     * Возврат данных из БД для произвольного запроса
+     *
+     * @param $sql Собственно запрос
+     * @param array $params Необязв\ательные параметры
+     * @return mixed Массив возврашаемых данных
+     */
     public function findBySql($sql, $params = [])
     {
         return $this->db->query($sql, $params);
     }
 
+    /**
+     * Возвращаем данные из БД с использованием LIKE для значения поля
+     *
+     * @param $pattern Паттерн
+     * @param $field Поле
+     * @return mixed Массив возвращаемых данных
+     */
     public function findByLike($pattern, $field)
     {
         $sql = "select * from {$this->table} where $field  like ?";
